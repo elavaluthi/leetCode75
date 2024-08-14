b1120a17a29f14370c12f6be7067a084d43c3606
elavaluthi <68210017+elavaluthi@users.noreply.github.com>
2024-08-14 15:19:49 +0530

    Update P6_L20_ValidParentheses.java

--- src/string/P6_L20_ValidParentheses.java
+++ src/string/P6_L20_ValidParentheses.java
@@ -1,44 +1,28 @@
 package string;
 
-import java.util.*;
 //Link:https://leetcode.com/problems/valid-parentheses/description/
 //TimeComplexity:O(n)
 //Type:Easy
-public class P6_L20_ValidParentheses {
+import java.util.Stack;
 
-	public static boolean isValidParantheses(String str) {
-		if(str.length()%2!=0) {
-			return false;
-		}
-		
-		Map<Character, Character> valueMap=new HashMap<>();
-		valueMap.put(')','(');
-		valueMap.put('}','{');
-		valueMap.put(']','[');
-		
-		Stack<Character> valueStack=new Stack<>();
-		
-		for(char c:str.toCharArray()) {
-			if(valueMap.containsKey(c)) {
-				char topElement=valueStack.isEmpty() ? '#':valueStack.pop();
-				if(valueMap.get(c)!=topElement) {
-					return false;
-				}
-			}else
-				valueStack.add(c);
-		}
-		
-		return valueStack.isEmpty();
-		
-	}
-	
-	public static void main(String args[]) {
-		String str1="(){}[]";
-		String str2="()[";
-		String str3="()]";
-		
-		System.out.println("validation result for str1:"+isValidParantheses(str1));
-		System.out.println("validation result for str2:"+isValidParantheses(str2));
-		System.out.println("validation result for str3:"+isValidParantheses(str3));
-	}
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            } else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            } else {
+                return false; // Mismatched or unbalanced parentheses
+            }
+        }
+        
+        return stack.isEmpty(); // Valid if stack is empty
+    }
 }
